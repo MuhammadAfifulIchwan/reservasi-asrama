039e6d6a9f408bd5b3e17203ab1db77a891f3ff1
Muhammad Afiful Ichwan <muha018009.mhs@ustjogja.ac.id>
2026-07-04 21:57:44 +0700

    perbaharui

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,19 +7,15 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 
-// =========================
+
 // AUTH
-// =========================
 $routes->get('login', 'AuthController::login');
 $routes->post('login/process', 'AuthController::processLogin');
 $routes->get('register', 'AuthController::register');
 $routes->post('register/store', 'AuthController::processRegister');
 $routes->get('logout', 'AuthController::logout');
 
-
-// =========================
 // FACILITIES
-// =========================
 $routes->get('facilities', 'FacilityController::index');
 $routes->get('facilities/create', 'FacilityController::create');
 $routes->post('facilities/store', 'FacilityController::store');
@@ -28,20 +24,14 @@ $routes->post('facilities/update/(:num)', 'FacilityController::update/$1');
 $routes->get('facilities/delete/(:num)', 'FacilityController::delete/$1');
 $routes->get('user/facilities', 'FacilityController::userFacilities');
 
-
-// =========================
 // RESERVATION
-// =========================
 $routes->get('reservations', 'ReservationController::index');
 $routes->get('my-reservation', 'ReservationController::myReservation');
 $routes->post('reservations/store', 'ReservationController::store');
 $routes->get('/reservations/status/(:num)/(:any)','ReservationController::updateStatus/$1/$2');
 $routes->get('reservations/create/(:num)', 'ReservationController::create/$1');
 
-
-// =========================
 // PAYMENT
-// =========================
 $routes->get('payments', 'PaymentController::index');
 $routes->post('payments/store', 'PaymentController::store');
 $routes->get('admin/payments', 'PaymentController::adminPayment');
@@ -49,9 +39,7 @@ $routes->get('test', function () {return view('test');});
 $routes->get('payments/status/(:num)/(:any)','PaymentController::updateStatus/$1/$2');
 $routes->get('payments/invoice/(:num)','PaymentController::downloadInvoice/$1');
 
-// =========================
 // DASHBOARD
-// =========================
 $routes->get('admin/dashboard', 'DashboardController::admin');
 $routes->get('user/dashboard', 'DashboardController::user');
 $routes->get('guest/dashboard', 'DashboardController::guest');
@@ -64,12 +52,7 @@ $routes->get('admin/report', 'DashboardController::report');
 $routes->get('admin/report/export-pdf', 'DashboardController::exportPdf');
 $routes->get('admin/report/export-excel', 'DashboardController::exportExcel');
 
-/*
-========================================
-REST API FACILITIES
-========================================
-*/
-
+// REST API FACILITIES
 $routes->get('api/facilities', 'API\FacilityApiController::index');
 
 $routes->post('api/facilities', 'API\FacilityApiController::create');
